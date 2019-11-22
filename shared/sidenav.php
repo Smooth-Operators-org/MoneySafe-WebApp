10037f0247436263d2aa6d159192402aa8163eac
DianaEla <46538920+DianaEla@users.noreply.github.com>
2019-11-22 12:42:43 -0500

    Insertar Categorías

--- shared/sidenav.php
+++ shared/sidenav.php
@@ -70,6 +70,10 @@
                 <i class="fas fa-file-invoice-dollar fa-lg white-text" id="icon_side"></i>
                 <span class="white-text" id="span_side">Gastos</span>
             </a>
+            <a href="modulos/categorias/categorias.php">
+                <i class="fas fa-file-invoice-dollar fa-lg white-text" id="icon_side"></i>
+                <span class="white-text" id="span_side">Categoría</span>
+            </a>
         </li>
         <li>
             <a href="/includes/close_session.php">
